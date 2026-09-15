d0f70288c2f6d44d69483b92e33b8478170186e5
ankj89 <51509357+ankj89@users.noreply.github.com>
2026-09-15 13:27:26 +0530

    Create questionBank.js

---
+++ questionBank.js
@@ -0,0 +1,26 @@
+/* ===========================================================
+   QUESTION BANK
+   Aggregates all trade datasets
+=========================================================== */
+
+const QUESTION_BANK = [
+
+    ...ELECTRICAL
+
+    /*
+    Future Datasets
+
+    ...CIVIL,
+
+    ...CARPENTRY,
+
+    ...FALSECEILING,
+
+    ...PLUMBING,
+
+    ...PAINTING,
+
+    ...HARDWARE
+    */
+
+];
